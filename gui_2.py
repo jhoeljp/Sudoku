4294931e7e5a7ad3c0dc7fcc9a946285d68503c2
Jhoel Perez <jhoel.jjpr@gmail.com>
2020-11-13 16:22:53 +0100

    unique inputs

--- gui_2.py
+++ gui_2.py
@@ -116,7 +116,7 @@ def approximate(pos):
     col = pos[0] // (WIDTH + MARGIN)
     row = pos[1] // (HEIGHT + MARGIN)
     old_row, old_col,draw = Sudoku_grid[col][row]
-    #approximate inner cell coordinates to center
+    #approximate inner cell coordinates to CENTER
     new_row = old_row + ((WIDTH-MARGIN)/4)
     new_col = old_col + ((HEIGHT-MARGIN)/4)
 
@@ -137,6 +137,11 @@ def draw(col,row,number,draw):
         DISPLAY.blit(text, (col,row))
         pygame.display.flip()
         pygame.display.update()
+#confirm right sudoku attempt
+def check_solver(solved_board, attempt, pos):
+    if solved_board[pos[0]][pos[1]] != attempt:
+        return False
+    return True
 
 #GUI ----------------------------
 #GUI imports
@@ -186,34 +191,40 @@ for row in range(N):
 for row in range(N):
     color = WHITE
     for col in range(N):
+        #EMPTY slot in board
         if OLD_board[row][col] != '0':
-            # Margin_1 =(MARGIN + WIDTH) * col + MARGIN
             Margin_1,Margin_2, can_draw = Sudoku_grid[row][col]
-            # Margin_2 = (MARGIN + HEIGHT) * row + MARGIN
-            # draw(grid_col,grid_row,OLD_board[row][col])
-            # print(OLD_board[row][col])
             grid_row, grid_col = approximate([Margin_1,Margin_2])
-            # print("%d %d" % (grid_row,grid_col))
-            # font = pygame.font.SysFont('arial', 30)
-            # text = font.render(str(OLD_board[row][col]), True, (0, 0, 0))
-            # # DISPLAY.blit(text, (pos[0],pos[1]))
-            # DISPLAY.blit(text, (grid_col,grid_row))
             draw(grid_col,grid_row,str(OLD_board[row][col]),can_draw)
             #update to not editable
             Sudoku_grid[row][col] = (Margin_1,Margin_2,False)
 
-        # pygame.display.flip()
-        # pygame.display.update()
+#MAIN GAME LOOP
+INPUT = 0
+#keep track of user's plays
+GAME_board = OLD_board
 
-#main game loop
 while True:
     for event in pygame.event.get():
+        attempt = False
         #QUIT
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        #try sudoku option
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+
+        #number pressed:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:INPUT = 1
+            if event.key == pygame.K_2:INPUT = 2
+            if event.key == pygame.K_3:INPUT = 3
+            if event.key == pygame.K_4:INPUT = 4
+            if event.key == pygame.K_5:INPUT = 5
+            if event.key == pygame.K_6:INPUT = 6
+            if event.key == pygame.K_7:INPUT = 7
+            if event.key == pygame.K_8:INPUT = 8
+            if event.key == pygame.K_9:INPUT = 9
+        #DRAW sudoku number pressed
+        if event.type == pygame.MOUSEBUTTONDOWN:
             # User clicks
             pos = pygame.mouse.get_pos()
             new_row, new_col = approximate_pos(pos)
@@ -221,24 +232,14 @@ while True:
             print("Click ", pos, "Grid coordinates: ", new_row, new_col)
             margin1, margin2, can_draw = Sudoku_grid[new_row][new_col]
 
-
+            row, col = approximate([margin1,margin2])
             #draw number
-            if can_draw:
-                draw(margin1,margin2,str(1),can_draw)
-            # font = pygame.font.SysFont('arial', 30)
-            # text = font.render(str(1), True, (0, 0, 0))
-            # # DISPLAY.blit(text, (pos[0],pos[1]))
-            # DISPLAY.blit(text, (new_col,new_row))
-            #
-            # pygame.display.update()
-
-            # Blit everything to the screen
-            # DISPLAY.blit(background, (0, 0))
-            # pygame.display.flip()
-
-    # Blit everything to the screen
-    # DISPLAY.blit(background, (0, 0))
-    # pygame.display.flip()
+            if can_draw and INPUT!=0:
+                if check_solver(SOLVED_board,str(INPUT),[new_row,new_col]):
+                    draw(col,row,str(INPUT),can_draw)
+                else:
+                    print("%d does not go @(%d,%d)"%(INPUT,new_row,new_col))
+                INPUT = 0
 
     # DISPLAY.blit(background, (0, 0))
     pygame.display.flip()
